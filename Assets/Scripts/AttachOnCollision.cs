c7aff2603cd4539c63d8aafd5f24f75dadc4ac5c
Chanchana Sornsoontorn <off9955555@gmail.com>
2016-10-05 04:00:24 +0700

    Add Assets/ folder

---
+++ Assets/Scripts/AttachOnCollision.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttachOnCollision : MonoBehaviour {
+
+	public string colliderTag = "MovingPlatform";
+	public float unattachDelay = 2f; // unattach if we depart from the collider for awhile
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag (colliderTag))
+		{
+			CancelInvoke ();
+			transform.SetParent (collision.transform);
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag (colliderTag))
+		{
+			Invoke ("Unattach", unattachDelay);
+		}
+	}
+
+	public void Unattach()
+	{
+		transform.SetParent (null);
+	}
+}
